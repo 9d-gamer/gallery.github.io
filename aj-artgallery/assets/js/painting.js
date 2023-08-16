cdccd72469242e1ab7edd0142c35584ce5c3918c
9dgamer <56701817+9d-gamer@users.noreply.github.com>
2023-08-17 02:19:29 +0500

    update

--- aj-artgallery/assets/js/painting.js
+++ aj-artgallery/assets/js/painting.js
@@ -10,16 +10,20 @@ function HandlePage(){
     const parameter1 = getParameterByName("param1", url)
     const parameter2 = getParameterByName("param2", url)
     const parameter3 = getParameterByName("param3", url)
+    const parameter7 = getParameterByName("param4", url)
     const parameter4 = getParameterByName("label1", url)
     const parameter5 = getParameterByName("label2", url)
     const parameter6 = getParameterByName("label3", url)
+    const parameter8 = getParameterByName("label4", url)
     verity = parameter1? parameter1 : 'all'
     types = parameter2? parameter2 : 'all'
     color = parameter3? parameter3 : 'all'
+    creator = parameter7? parameter7 : 'all'
     const verity_label = parameter4? parameter4 : 'All'
     const types_label = parameter5? parameter5 : 'All'
     const color_label = parameter6? parameter6 : 'All'
-    HandleFilters(verity_label, types_label, color_label);
+    const creator_label = parameter8? parameter8 : 'All'
+    HandleFilters(verity_label, types_label, color_label,creator_label);
     HandleFiltersButtons()
     HandlePaintings(verity,types,color,creator)
     handleDarkMode();
@@ -29,8 +33,7 @@ function HandlePage(){
     }, 200);
 }
 
-function HandleFilters(a,b,c){
-    console.log(a,b,c)
+function HandleFilters(a,b,c,d){
     const filters = Data.filters
     const verity = filters.verity
     const types  = filters.types
@@ -161,7 +164,7 @@ function HandleFilters(a,b,c){
             <div class="dropdown">
             <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton4"
                 data-bs-toggle="dropdown" aria-expanded="false">
-                All
+                ${d}
             </button>
               <ul class="dropdown-menu bg-light shadow-light mt-4" aria-labelledby="dropdownMenuButton4" id="dropdown-menu-3">
               ${data.artist.join('')}
@@ -204,6 +207,16 @@ function HandleFiltersButtons(){
     })
 }
 
+function GetArtistDetails(artist){
+    const artists = Data.filters.artists;
+    for (let i = 0; i < artists.length; i++){
+        const e = artists[i]
+        if (e.id == artist) {
+            return e
+        }
+    }
+}
+
 function HandlePaintings(a,b,c,d){
     $('.loading-container2').show();
     $('#Paintings-Container').hide();
@@ -212,6 +225,8 @@ function HandlePaintings(a,b,c,d){
     for (let i = 0; i < Paintings.length; i++){
         const e = Paintings[i]
         if ((e.filter.verity == a || a == 'all') && (e.filter.Type == b || b == 'all') && (e.filter.Color == c || c == 'all') && (e.filter.artist == d || d == 'all')){
+            const artest = GetArtistDetails(e.filter.artist)
+            console.log(e.artist)
             const item = `
             <div class="col">
                 <div class="card addHover-light p-3 mb-5 bg-light rounded border border-dark2">
@@ -239,6 +254,10 @@ function HandlePaintings(a,b,c,d){
                     <button type="button" class="btn btn-light float-md-center text-dark"
                         onclick="onRead('${e.image}','${e.Image_alt}','${e.Title}','${e.description}','${e.filter.verity_label}','${e.filter.Type_label}','${e.filter.Color_label}','${e.price}',\`${e.detail}\`)">
                         Read More
+                    </button>
+                    <button type="button" class="btn btn-light float-md-center text-dark"
+                        onclick="onReadArtist('${artest.id}','${artest.aria_label}','${artest.Image}','${artest.image_alt}','${artest.Description}')">
+                        About Artist
                     </button>
                     <button type="button" class="mt-3 btn btn-light float-md-center text-dark"
                         onclick="OnDownload('${e.image}', '${e.Title} // ${e.description}.jpg')">
@@ -292,6 +311,67 @@ function HandlePaintings(a,b,c,d){
     
     $('.loading-container2').hide();
     $('#Paintings-Container').show();
+}
+
+function onReadArtist(id,name,image,image_alt,description){
+    if (DarkMode) {
+        const html = `
+        <div class="container">
+        <div class="row">
+            <div class="col">
+                <button class="btn btn-light" onclick="GOBack()">&lt;&lt;&lt; Go Back</button>
+                
+            </div>
+            <div class="col">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <a href="/painting.html?param1=all&param2=all&param3=all&param4=${id}&label1=All&label2=All&label3=All&label4=${name}"><button class="btn btn-light">See all painting from this artist</button><a>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="row row-cols-1 row-cols-md-2 m-4">
+            <div class="col">
+                <img src="${image}" class="img-fluid rounded mx-auto mt-5 pt-5 pb-5 mt-5 d-block shadow-dark" alt="${image_alt}" />
+            </div>
+            <div class="col text-light">
+                <p class="fs-1 text-center ">${name}</p>
+                <br><br>
+                <p class="fs-2">Detail:</p>
+                <p class="fs-4">${description}</p>
+            </div>
+        </div>
+    </div>
+        `
+        const paintingContainer = document.getElementById('Painting-Details');
+        paintingContainer.innerHTML = html;
+    } else {
+        const html = `
+    <div class="container">
+    <button class="btn btn-light" onclick="GOBack()">&lt;&lt;&lt; Go Back</button>
+    <div class="row row-cols-1 row-cols-md-2 m-4">
+        <div class="col">
+            <img src="${image}" class="img-fluid rounded mx-auto mt-5 pt-5 pb-5 mt-5 d-block shadow-light" alt="${image_alt}" />
+        </div>
+        <div class="col">
+            <p class="fs-1 text-center">${name}</p>
+            <br><br>
+            <p class="fs-2">Detail:</p>
+            <p class="fs-4">${description}</p>
+        </div>
+    </div>
+</div>
+    `
+    const paintingContainer = document.getElementById('Painting-Details');
+    paintingContainer.innerHTML = html;
+    }
+    previousScrollPosition = window.scrollY;
+    $('#Paintings-Container').hide();
+    $('#Painting-Details').show();
+    $('.goback').show();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Use 'auto' for instant scroll
+    });
 }
 
 function onRead(Image, Image_alt, Title, description, Verity, Types, Colors, Price, Detail){
